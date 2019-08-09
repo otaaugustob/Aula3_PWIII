3b0f6f9c4f4bf78477a919303f12463b68d6225c
Otavio <otavioabottin@outlook.com>
2019-08-09 20:38:49 -0300

    Aula03 - PWIII

---
+++ exercicio1.ts
@@ -0,0 +1,40 @@
+let nota1 = 84
+
+/*let fconceito = function (n1:number){
+
+    if(nota1 == 0){
+        console.log("E")
+    }
+    else if(nota1 >=1 && nota1 <= 35 ){
+        console.log("D")
+    }
+    else if(nota1 >=36 && nota1 <=60){
+        console.log("C")
+    }
+    else if(nota1 >= 61 && nota1 <= 85){
+        console.log("B")
+    }
+    else {
+        console.log("A")
+    }
+    */
+   let fconceito = (n1:number)=> {
+
+    if(nota1 == 0){
+        console.log("E")
+    }
+    else if(nota1 >=1 && nota1 <= 35 ){
+        console.log("D")
+    }
+    else if(nota1 >=36 && nota1 <=60){
+        console.log("C")
+    }
+    else if(nota1 >= 61 && nota1 <= 85){
+        console.log("B")
+    }
+    else {
+        console.log("A")
+    }
+   }
+
+fconceito(nota1)
